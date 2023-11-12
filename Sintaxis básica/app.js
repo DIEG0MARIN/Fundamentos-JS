9fd82b594827a55873a31e844bdc5a6ab649de54
DIEG0MARIN <diegof.maring@konradlorenz.edu.co>
2023-11-11 20:48:58 -0500

    Primer commit

---
+++ Sintaxis básica/app.js
@@ -0,0 +1,34 @@
+// para definir una variable global utilzo var
+var nombreVariable="Hola esta es mi variable"
+// para imprimir el valor de la variable en consola utilizo console.log
+console.log(nombreVariable)
+Variable ="Segundo nomre de la variable"
+console.log(Variable)
+
+// Variables Locales
+// un bloque de codigo es cualquier codigo que este encerrado entre llaves
+{
+    let variableLocal = "primer valor"
+    console.log(variableLocal)
+
+    var variableGlobal = "VariableGlobal"
+}
+console.log(variableGlobal)
+
+let numero = 14;
+// para los booleanos tenemos dos posibles valores True y False
+let booleano = false                
+let caracteres = "hola mundo"                           
+
+var edad =23
+{
+    var edad =23
+    console.log(edad)
+}
+console.log(edad)
+
+//CONSTANTES
+
+const PI =3.141592
+console.log(PI)
+//PI =3.95 LAS CONSTANTES NO CAMBIAN SU VALOR
