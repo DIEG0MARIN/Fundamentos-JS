3c520c60dc4381530b026d16ef801a788ca2a077
DIEG0MARIN <diegof.maring@konradlorenz.edu.co>
2023-11-13 10:38:01 -0500

    Carpeta ejercicio3

---
+++ Ejercicios-JavaScript/Ejercicio3/app.js
@@ -0,0 +1,11 @@
+/* Según lo anterior haga un código que:
+1. Imprima los números del 10 al 1.
+2. Imprima los números pares. */
+let numero = 10;
+while(numero >=1){
+    if(numero %2 == 0){
+        console.log(numero)
+    }
+    numero --;
+
+}
