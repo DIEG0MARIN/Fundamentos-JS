47aecf233a959f0b14969c7166cca3fc4cedf1fd
DIEG0MARIN <diegof.maring@konradlorenz.edu.co>
2023-11-13 09:26:08 -0500

    planteamiento de ejercicio3

--- Ciclos While -For/app.js
+++ Ciclos While -For/app.js
@@ -6,5 +6,12 @@ let numero = 0;
 //imprimir todos los números hasta el 10
 while(numero <=10){
     console.log(`El número va en ${numero}`)
-    numero++
+    numero++;
 }
+
+// FOR -> se ejecuta un numero determinado de veces
+for (let i= 0; i<=18; i++){
+console.log("Mensaje del for",i)
+}
+
+    
