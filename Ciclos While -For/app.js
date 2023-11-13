ec7b02268307a2fe907da168cf2aa176a7041b6d
DIEG0MARIN <diegof.maring@konradlorenz.edu.co>
2023-11-13 08:28:54 -0500

    Ejemplo de ciclos

--- Ciclos While -For/app.js
+++ Ciclos While -For/app.js
@@ -1,1 +1,10 @@
 /*Comentarios de bloque  */
+
+// hacer un ciclo que mientars que la opcionn no se igual a 0 me ejecute un bloque de codigo
+
+let numero = 0;
+//imprimir todos los números hasta el 10
+while(numero <=10){
+    console.log(`El número va en ${numero}`)
+    numero++
+}
